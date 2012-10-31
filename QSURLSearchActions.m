0b327bfc58f7b3341e681a0a21b71912bb7d4736
Rob McBroom <github@skurfer.com>
2012-10-31 09:21:14 -0400

    flip on the “mnemonics only” flag
    This mimics the yet to be merged `scoredArrayForType:` method.

--- QSURLSearchActions.m
+++ QSURLSearchActions.m
@@ -21,7 +21,8 @@
 - (NSArray *)validIndirectObjectsForAction:(NSString *)action directObject:(QSObject *)dObject{
 	// if it's a 'find with...' action, only return search URLs
 	if ([action isEqualToString:kURLFindWithAction]) {
-		return [QSLib scoredArrayForString:nil inSet:[QSLib arrayForType:QSSearchURLType]];
+        // change to scoredArrayForType: when quicksilver/Quicksilver#1162 is merged
+        return [QSLib scoredArrayForString:nil inSet:[QSLib arrayForType:QSSearchURLType] mnemonicsOnly:YES];
 	}
 	// if it's a 'search for...' action, return a text bot for text
 	else if ([action isEqualToString:kURLSearchForAction] || [action isEqualToString:kURLSearchForAndReturnAction]){
